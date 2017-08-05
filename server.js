f1693d27011a3984900ff5ae0ffcfa55ffce5540
variganjis <variganjis@users.noreply.github.com>
2017-08-05 16:07:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two',function(req,res){
-   res.send('article one is requested and response is served here'); 
+   res.send('article two is requested and response is served here'); 
 });
 
 app.get('/ui/style.css', function (req, res) {
