94c9e18c62ae2c58603b41350b7dca359ef8557a
variganjis <variganjis@users.noreply.github.com>
2017-08-05 22:11:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,17 +9,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/template-one',function(req,res){
+   res.send('template one is requested and response is served here'); 
+});
+
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-two',function(req,res){
-   res.send('article two is requested and response is served here'); 
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-
-app.get('/article-three',function(req,res){
-    res.send('article three is requested and response is served here');
+app.get('/article-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
