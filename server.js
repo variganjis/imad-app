56fde60ec00ca858769d914e9bbe4e2d14a784c4
variganjis <variganjis@users.noreply.github.com>
2017-08-05 22:41:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ function templateContent(data)
     var htmlData = 
     `<html>
 <head>
-    <title> ${tiltle}</title>
+    <title> ${title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="ui/style.css" rel="stylesheet" />
 </head>
