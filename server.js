54bffaed5a166fa26ff9b3c3d9ce42860276069e
variganjis <variganjis@users.noreply.github.com>
2017-08-11 12:52:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/test-db', function (req, res) {
       }
       else
       {
-          res.send(JSON.stringify(result));
+          res.send(JSON.stringify(result.rows));
       }
   });
   //res.send('got test-db request and serving response here..');
