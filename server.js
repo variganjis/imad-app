bbc2b2c15f3c09e023408ce7be8925e0acc4ccaa
variganjis <variganjis@users.noreply.github.com>
2017-08-11 15:48:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get('/test-db', function (req, res) {
 app.get('/:articleName', function (req, res) {
     // articleName - name of article one
     // articles[articleName] - content of the article one
-    pool.query("SELECT * FROM articles WHERE title = '" + article-one + "'",  function(err,result){
+    pool.query("SELECT * FROM articles WHERE title ='article-one'",  function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }else{
