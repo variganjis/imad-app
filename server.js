fe6cf77e7a972bcd5eedc370dbe397d58d91cd67
variganjis <variganjis@users.noreply.github.com>
2017-08-11 13:23:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,13 +115,6 @@ app.get('articles/:articleName', function (req, res) {
     res.send(templateContent(articles[articleName]));
 });
 
-app.get('/:articleName', function (req, res) {
-    // articleName - name of article one
-    // articles[articleName] - content of the article one
-    var articleName=req.params.articleName;
-    res.send(templateContent(articles[articleName]));
-});
-
 app.get('/template-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
