ff88fbe01950e2d306274f19d64bd7a46ccb3ae4
variganjis <variganjis@users.noreply.github.com>
2017-08-17 16:47:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,38 @@
 console.log('Loaded!');
+
+function onBtnCounter()
+{
+    var request = new XMLHttpRequest();
+    request.onreadystatuschange = function()
+    {
+        console.log('on ready staus chnage');
+        if ( readystatus === XMLHttpRequest.DONE )
+            {
+                if ( request.status === 200)
+                    {
+                        console.log('trying to set counter value');
+                        var count = request.responseText;
+                        document.getElementById('counter').innerText = count.toString();                    
+                    }
+            }
+    }
+    request.open('GET', 'http://localhost:8080/counter', true);
+    request.send(null);   
+}
+
+function myClick()
+{
+    alert('Testting button click');
+}
+
+function onUserClick()
+{
+    alert('onUserClick');
+    ipUser.clear();
+}
+
+function onPwdClick()
+{
+    alert('onPwdClick');
+    ipPwd.clear();
+} 
